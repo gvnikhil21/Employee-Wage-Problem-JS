2c37974c2d2fbb57b7cad46f74667e57188d47e8
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-08 16:26:24 +0530

    Added: Calculate monthly wage assuming 20 working days a month functionality added

--- employeeWage.js
+++ employeeWage.js
@@ -3,18 +3,23 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_WORKING_DAYS = 20;
 
 function getEmpWorkingHours() {
     switch (empCheck) {
         case IS_PART_TIME:
-           return PART_TIME_HOURS;
+            return PART_TIME_HOURS;
         case IS_FULL_TIME:
             return FULL_TIME_HOURS;
         default:
             return 0;
     }
 }
-let empCheck = Math.floor(Math.random() * 10) % 3;
-let empHours = getEmpWorkingHours();
+let empHours = 0;
+let empCheck = 0;
+for (let dayCount = 0; dayCount < NUMBER_WORKING_DAYS; dayCount++) {
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    empHours += getEmpWorkingHours();
+}
 let empWage = empHours * WAGE_PER_HOUR;
-console.log("Employee wage: " + empWage);
+console.log("Total hours: " + empHours + " Employee wage: " + empWage);
