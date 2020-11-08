0ae6a45b85af27baadc8f17cd945c1f888a85110
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-08 16:40:18 +0530

    Added: Calculate employee wage till a condition of total 160 hours or total 20 working days is reached functionality added

--- employeeWage.js
+++ employeeWage.js
@@ -3,7 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUMBER_WORKING_DAYS = 20;
+const MONTHLY_WORKING_DAYS = 20;
+const MONTHLY_WORKING_HOURS = 160
 
 function getEmpWorkingHours() {
     switch (empCheck) {
@@ -17,9 +18,13 @@ function getEmpWorkingHours() {
 }
 let empHours = 0;
 let empCheck = 0;
-for (let dayCount = 0; dayCount < NUMBER_WORKING_DAYS; dayCount++) {
+let dayCount = 0;
+while (dayCount < MONTHLY_WORKING_DAYS && empHours <= MONTHLY_WORKING_HOURS) {
+    dayCount++;
     empCheck = Math.floor(Math.random() * 10) % 3;
     empHours += getEmpWorkingHours();
 }
+if (empHours > MONTHLY_WORKING_HOURS)
+    empHours = MONTHLY_WORKING_HOURS;
 let empWage = empHours * WAGE_PER_HOUR;
-console.log("Total hours: " + empHours + " Employee wage: " + empWage);
+console.log("Total days: " + dayCount + " Total hours: " + empHours + " Employee wage: " + empWage);
