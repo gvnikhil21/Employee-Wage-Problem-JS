a2893826cd2cf5e60b67fe45584179bddaca91b2
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-11-10 10:20:36 +0530

    Added: Calculate total employee wage using arrays' for-each and reduce method

--- employeeWage.js
+++ employeeWage.js
@@ -7,7 +7,8 @@
     const MONTHLY_WORKING_DAYS = 20;
     const MONTHLY_WORKING_HOURS = 160
 
-    function getEmpWorkingHours() {
+    // calculates employee working hours
+    function getEmpWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
                 return PART_TIME_HOURS;
@@ -18,8 +19,20 @@
         }
     }
 
+    // calculates employee daily wage
     function calculateDailyWage(empHours) {
         return empHours * WAGE_PER_HOUR;
+    }
+
+    // calculates total employee wage
+    let totalEmpWage = 0;
+    function sum(dailyWage) {
+        totalEmpWage += dailyWage;
+    }
+
+    // calculates total employee wage
+    function totalWageCal(totalWage, dailyWage) {
+        return totalWage + dailyWage;
     }
 
     let totalEmpHours = 0;
@@ -28,10 +41,17 @@
     while (dayCount < MONTHLY_WORKING_DAYS && totalEmpHours <= MONTHLY_WORKING_HOURS) {
         dayCount++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
-        let empHours = getEmpWorkingHours();
+        let empHours = getEmpWorkingHours(empCheck);
         totalEmpHours += empHours;
         empDailyWageArray.push(calculateDailyWage(empHours));
     }
     let empWage = calculateDailyWage(totalEmpHours);
     console.log("Total days: " + dayCount + " Total hours: " + totalEmpHours + " Employee wage: " + empWage);
+
+    // calculates total wage using array for-each method
+    empDailyWageArray.forEach(sum);
+    console.log("Total employee wage using for-each: " + totalEmpWage);
+
+    // calculates total wage using array reduce method
+    console.log("Total employee wage using reduce: " + empDailyWageArray.reduce(totalWageCal, 0));
 }
